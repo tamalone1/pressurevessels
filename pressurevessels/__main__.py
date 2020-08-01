4ca2f0817e635ce814906d7b0611a05fc9f4d86c
tamalone1 <65259148+tamalone1@users.noreply.github.com>
2020-08-01 14:51:27 -0500

    Add init and main modules

---
+++ pressurevessels/__main__.py
@@ -0,0 +1,9 @@
+# make the module executable
+# at which point, the GUI version is run
+import tkinter as tk
+from PressureVessels import PV_GUI
+
+root = tk.Tk()
+root.title('Pressure Vessels')
+PV_GUI(root).grid(row=0, column=0, padx=2, pady=2)
+root.mainloop()
