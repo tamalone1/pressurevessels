efdf2b669e4f3eaa4cf014504ae0eb3ea32bfa70
tamalone1 <65259148+tamalone1@users.noreply.github.com>
2020-08-05 22:21:38 -0500

    Add tests for calculating and modifying params

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -28,6 +28,26 @@ class Test_Vessel(unittest.TestCase):
         self.assertEqual(vm(80,80,0), 80)
         self.assertEqual(vm(80,-80,80), 160)
 
+    def test_defaultvessel(self):
+        self.assertAlmostEqual(self.test_vessel.maxstress, 100.676, 3)
+        self.assertAlmostEqual(self.test_vessel.averagestress, 87.685, 3)
+    
+    def test_externalcheck(self):
+        self.assertTrue(self.test_vessel.external)
+
+    def test_modify_parameters(self):
+        # Check initial value
+        self.assertEqual(self.test_vessel.pExt, 15)
+        # Take note of the current safety factor
+        sf_0 = self.test_vessel.SF_room
+        # Change external pressure value
+        self.test_vessel.modify_parameters(pExt=20)
+        # Take note of the current safety factor
+        sf_0 = self.test_vessel.maxstress
+        # Check that the value changed
+        self.assertEqual(self.test_vessel.pExt, 20)
+        # Check that the safety factor decreased
+        self.assertLess(self.test_vessel.SF_room, sf_0)
 
 class Test_GUI(unittest.TestCase):
     ''' Subclass of TestCase, which is executed by unittest.main
