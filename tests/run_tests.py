2c474ccfee3962b53c52b9dcf66e1f3182b2a4fd
tamalone1 <65259148+tamalone1@users.noreply.github.com>
2020-08-13 17:30:53 -0500

    Added unit conversion test (currently failing)

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,21 +7,13 @@ Copyright (c) 2020 tamalone1
 
 import unittest
 from pressurevessels.PressureVessels import Vessel
+from pressurevessels import conversions
 
 class Test_Vessel(unittest.TestCase):
-    ''' Subclass of TestCase, which is executed by unittest.main
-
-    Each test is a method with a name starting with 'test'.
-    The setUp method is run prior to each test.
-    The tearDown method is run after each test.
-    '''
 
     def setUp(self):
         self.defaultvalues = (15, 0, 1.695, 1.460, 120, 116)
         self.test_vessel = Vessel(*self.defaultvalues)
-
-    def tearDown(self):
-        pass
 
     def test_vonmises(self):
         vm = self.test_vessel._vonmises
@@ -49,20 +41,53 @@ class Test_Vessel(unittest.TestCase):
         # Check that the safety factor decreased
         self.assertLess(self.test_vessel.SF_room, sf_0)
 
-class Test_GUI(unittest.TestCase):
-    ''' Subclass of TestCase, which is executed by unittest.main
-
-    Each test is a method with a name starting with 'test'.
-    The setUp method is run prior to each test.
-    The tearDown method is run after each test.
-    '''
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
+    def test_change_units(self):
+        starting_parameters = self.test_vessel.__dict__
+        vessel_parameters = [('pExt', 'pressure'),
+                        ('pInt', 'pressure'),
+                        ('OD', 'length'),
+                        ('ID', 'length'),
+                        ('yieldstress', 'pressure'),
+                        ('deratedyieldstress', 'pressure')]
+        if starting_parameters['units'] == 'US':
+            # Convert to SI
+            self.test_vessel.change_units('SI')
+            # Confirm the conversion label happened
+            self.assertEqual(self.test_vessel.units, 'SI')
+            # Check each vessel parameter was converted properly
+            for param_name, unit_type in vessel_parameters:
+                    # subTest context identifies which parameter failed
+                    with self.subTest(parameter=param_name):
+                        starting_value = starting_parameters[param_name]
+                        if unit_type == 'length':
+                            factor = 25.4   # mm
+                        elif unit_type == 'pressure':
+                            factor = 0.006895   # MPa
+                        # Determine the correct converted value
+                        expected_value = starting_value * factor
+                        # Actual test statement
+                        new_value = self.test_vessel.__dict__[param_name]
+                        self.assertAlmostEqual(new_value, expected_value, 3)
+                        
+        elif starting_parameters['units'] == 'SI':
+            # Convert to US
+            self.test_vessel.change_units('US')
+            # Confirm the conversion label happened
+            self.assertEqual(self.test_vessel.units, 'US')
+            # Check each vessel parameter was converted properly
+            for param_name, unit_type in vessel_parameters:
+                    # subTest context identifies which parameter failed
+                    with self.subTest(parameter=param_name):
+                        starting_value = starting_parameters[param_name]
+                        if unit_type == 'length':
+                            factor = 1/25.4   # mm
+                        elif unit_type == 'pressure':
+                            factor = 1/0.006895   # MPa
+                        # Determine the correct converted value
+                        expected_value = starting_value * factor
+                        # Actual test statement
+                        new_value = self.test_vessel.__dict__[param_name]
+                        self.assertAlmostEqual(new_value, expected_value, 3)
 
 if __name__ == '__main__':
     unittest.main()
